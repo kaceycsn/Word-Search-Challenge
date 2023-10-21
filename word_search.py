adc57e1c2714df9f9b0e434c39a1b41c77d94efd
kaceycsn <119840829+kaceycsn@users.noreply.github.com>
2023-10-21 17:51:01 +0100

    Fix out of bounds bug
    Fixed an out of bounds bug which occurs when attempting to search for the next character beyond the boundaries of the puzzle grid

--- word_search.py
+++ word_search.py
@@ -19,14 +19,20 @@ class WordSearch:
         ans = []
 
         def findNextChar(point, word, direction):
+            print("=====FINDNEXTCHAR=====")
             next_point = Point(point.x + direction[0], point.y + direction[1])
-            if self.puzzle[next_point.x][next_point.y] == word[0]:
+
+            if 0 <= next_point.x < cols and 0 <= next_point.x < rows and self.puzzle[next_point.y][next_point.x] == word[0]:
                 remaining_word = word[1:]
+                print("MATCHED: ", self.puzzle[next_point.y][next_point.x])
+                print("REMAINING WORD: ", remaining_word)
 
                 if not remaining_word:
+                    print("NOT REMAINING WORD")
                     return next_point
                 
                 else:
+                    print("NEW ITERATION")
                     return findNextChar(next_point, remaining_word, direction)
 
             else:
@@ -34,27 +40,50 @@ class WordSearch:
         
         for i in range(rows):
             for j in range(cols):
-                grid_pointer = Point(i, j)
-                if self.puzzle[grid_pointer.x][grid_pointer.y] == word[0]:
+                grid_pointer = Point(j, i)
+
+                print("grid_word: ", self.puzzle[grid_pointer.y][grid_pointer.x])
+
+                if self.puzzle[grid_pointer.y][grid_pointer.x] == word[0]:
+                    print("MATCH!! word[0]: ", word[0])
+
                     remaining_word = word[1:]
 
                     if not remaining_word:
                         ans.append([grid_pointer, grid_pointer])
 
                     else:
-                        for move in self.moves:
-                            direction = move
-                            next_point = Point(grid_pointer.x + move[0], grid_pointer.y + move[1])
-                            if self.puzzle[next_point.x][next_point.y] == remaining_word[0]:
+                        for direction in self.moves:
+                            print("direction: ", direction)
+
+                            next_point = Point(grid_pointer.x + direction[0], grid_pointer.y + direction[1])
+                            print("next_point,y", next_point.y)
+                            print("next_point,x", next_point.x)
+                            
+
+                            if next_point.x < 0 or next_point.x >= cols or next_point.y < 0 or next_point.y >= rows:
+                                print("SKIP")
+                                continue
+                            
+                            print("next_point_char: ", self.puzzle[next_point.y][next_point.x])
+
+                            if self.puzzle[next_point.y][next_point.x] == remaining_word[0]:
+                                print("SELECTED: ", self.puzzle[next_point.y][next_point.x])
+                                
+
                                 remainder_word = remaining_word[1:]
+                                print("remainder_word: ", remainder_word)
 
                                 if not remainder_word:
                                     ans.append([grid_pointer, next_point])
 
                                 else:
+                                    print("HERE")
                                     sol = findNextChar(next_point, remainder_word, direction)
                                     if sol:
-                                        ans.append([grid_pointer, sol])
+                                        # ans.append([grid_pointer, sol])
+                                        ans.append([(grid_pointer.x, grid_pointer.y), (sol.x, sol.y)])
+                                        return ans
                                     else:
                                         break
         print(ans)
