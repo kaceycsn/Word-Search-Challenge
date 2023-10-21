11205ad0476ba4f1f3a3bfef0b77943e70afc978
kaceycsn <119840829+kaceycsn@users.noreply.github.com>
2023-10-21 22:56:42 +0100

    Modified search function to return tuples
    Modified the search function in the WordSearch class to return word positions as tuples instead of a list. This change ensures compatibility with the existing test cases, which expect tuples as output.

--- word_search.py
+++ word_search.py
@@ -10,29 +10,23 @@ class Point:
 class WordSearch:
     def __init__(self, puzzle):
         self.puzzle = puzzle
-        print(puzzle)
         self.moves = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
 
     def search(self, word):
         rows = len(self.puzzle)
         cols = len(self.puzzle[0])
-        ans = []
+        ans = None
 
         def findNextChar(point, word, direction):
-            print("=====FINDNEXTCHAR=====")
             next_point = Point(point.x + direction[0], point.y + direction[1])
 
             if 0 <= next_point.x < cols and 0 <= next_point.y < rows and self.puzzle[next_point.y][next_point.x] == word[0]:
                 remaining_word = word[1:]
-                print("MATCHED: ", self.puzzle[next_point.y][next_point.x])
-                print("REMAINING WORD: ", remaining_word)
 
                 if not remaining_word:
-                    print("NOT REMAINING WORD")
                     return next_point
                 
                 else:
-                    print("NEW ITERATION")
                     return findNextChar(next_point, remaining_word, direction)
 
             else:
@@ -42,50 +36,32 @@ class WordSearch:
             for j in range(cols):
                 grid_pointer = Point(j, i)
 
-                print("grid_word: ", self.puzzle[grid_pointer.y][grid_pointer.x])
-
                 if self.puzzle[grid_pointer.y][grid_pointer.x] == word[0]:
-                    print("MATCH!! word[0]: ", word[0])
-
                     remaining_word = word[1:]
 
                     if not remaining_word:
-                        ans.append([grid_pointer, grid_pointer])
+                        ans = (grid_pointer, grid_pointer) 
 
                     else:
                         for direction in self.moves:
-                            print("direction: ", direction)
-
                             next_point = Point(grid_pointer.x + direction[0], grid_pointer.y + direction[1])
-                            print("next_point,y", next_point.y)
-                            print("next_point,x", next_point.x)
-                            
-
+             
                             if next_point.x < 0 or next_point.x >= cols or next_point.y < 0 or next_point.y >= rows:
-                                print("SKIP")
                                 continue
                             
-                            print("next_point_char: ", self.puzzle[next_point.y][next_point.x])
-
                             if self.puzzle[next_point.y][next_point.x] == remaining_word[0]:
-                                print("SELECTED: ", self.puzzle[next_point.y][next_point.x])
-                                
-
                                 remainder_word = remaining_word[1:]
-                                print("remainder_word: ", remainder_word)
-
+                         
                                 if not remainder_word:
-                                    ans.append([grid_pointer, next_point])
+                                    ans = (grid_pointer, next_point)
 
                                 else:
-                                    print("HERE")
                                     sol = findNextChar(next_point, remainder_word, direction)
                                     if sol:
-                                        ans.append([(grid_pointer.x, grid_pointer.y), (sol.x, sol.y)])
+                                        ans = (grid_pointer, sol)
                                         return ans
                                     else:
                                         continue
-        print(ans)
         return ans
     
     
